e900240db7b8533efe6b35231641ba920edbf4dc
DionKelmendi <108992835+DionKelmendi@users.noreply.github.com>
2023-04-27 22:05:15 +0200

    Fixed connection between react and django

--- lab1/autosallon/autosallonapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import main, prova
-
-urlpatterns = [
-    path('', main, name='index'),
-    path('prova/', prova, name='prova'),
-]
--- lab1/autosallon/autosallonapp/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def main(request):
-  return render(request, 'views/index.html')
-
-def prova(request):
-  return render(request, 'views/prova.html')
