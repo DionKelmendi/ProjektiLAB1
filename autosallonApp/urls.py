c78545f7d5f8f9301aaa75d4d4bf9a83213006bb
gemata <bp55994@ubt-uni.net>
2023-05-05 22:03:18 +0200

    added WorkerCRUD

--- autosallonApp/urls.py
+++ autosallonApp/urls.py
@@ -1,12 +1,22 @@
-from .views import carViews ,userViews, saleViews 
+from .views import carViews ,userViews, saleViews, workerViews, contactInfoViews, favoriteViews
 from django.urls import path
 
 urlpatterns = [
     
+    path('user/', userViews.UserAPIView.as_view()),
+    path('user/<int:pk>/', userViews.UserDetailAPIView.as_view()),
+    path('user/<int:pk>/update', userViews.UserUpdateAPIView.as_view()),
+    path('user/<int:pk>/destroy', userViews.UserDestroyAPIView.as_view()),
+
     path('sale/', saleViews.SaleAPIView.as_view()),
     path('sale/<int:pk>/', saleViews.SaleDetailAPIView.as_view()),
     path('sale/<int:pk>/update', saleViews.SaleUpdateAPIView.as_view()),
     path('sale/<int:pk>/destroy', saleViews.SaleDestroyAPIView.as_view()),
 
+path('worker/', workerViews.WorkerAPIView.as_view()),
+    path('worker/<int:pk>/', workerViews.WorkerDetailAPIView.as_view()),
+    path('worker/<int:pk>/update', workerViews.WorkerUpdateAPIView.as_view()),
+    path('worker/<int:pk>/destroy', workerViews.WorkerDestroyAPIView.as_view()),
+    
 
 ]
---
+++ autosallonApp/views/workerViews.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render
+from django.shortcuts import redirect
+from rest_framework import generics
+from ..serializers import WorkerSerializer
+from ..models import Worker
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+
+# Create your views here.
+
+#Worker CRUD
+#Worker Read One Instance
+class WorkerDetailAPIView(generics.RetrieveAPIView):
+  queryset = Worker.objects.all()
+  serializer_class = WorkerSerializer
+
+#Worker Read All Instances and Create    
+class WorkerAPIView(generics.ListCreateAPIView):
+  queryset = Worker.objects.all()
+  serializer_class = WorkerSerializer
+
+  def perform_create(self, serializer):
+    name = serializer.validated_data.get('name')
+    email = serializer.validated_data.get('email')
+    address = serializer.validated_data.get('address')
+    phone = serializer.validated_data.get('phone')
+    salary = serializer.validated_data.get('salary')
+    position = serializer.validated_data.get('position')
+    serializer.save()
+
+#Worker Update
+class WorkerUpdateAPIView(generics.UpdateAPIView):
+  queryset = Worker.objects.all()
+  serializer_class = WorkerSerializer
+  lookup_field = 'pk'
+
+  def perform_update(self, serializer):
+    instance = serializer.save()
+
+#Worker Destroy
+class WorkerDestroyAPIView(generics.DestroyAPIView):
+    queryset = Worker.objects.all()
+    serializer_class = WorkerSerializer
+    lookup_field = 'pk'
+
+    def perform_destroy(self, instance):
+        super().perform_destroy(instance)
