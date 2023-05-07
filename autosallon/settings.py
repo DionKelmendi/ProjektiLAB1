8d38357bcff983fe07113b407aae4869cb54e374
DionKelmendi <108992835+DionKelmendi@users.noreply.github.com>
2023-05-07 12:49:52 +0200

    Fixed Database Routing, added Vehicle Template

--- autosallon/settings.py
+++ autosallon/settings.py
@@ -77,12 +77,19 @@ WSGI_APPLICATION = 'autosallon.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'autosallon',  
+        'USER': 'root',  
+        'PASSWORD': '',  
+        'HOST': '127.0.0.1',  
+        'PORT': '3306',  
+        'OPTIONS': {  
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+        }  
+    }
+}  
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
