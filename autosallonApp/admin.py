4cbd1624275f82b8c67598033b9a6fed8aab93c3
gemata <bp55994@ubt-uni.net>
2023-05-09 18:15:29 +0200

    Added models in django administrator

--- autosallonApp/admin.py
+++ autosallonApp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from . import models
+
+admin.site.register(models.User)
+admin.site.register(models.Car)
+admin.site.register(models.Category)
+admin.site.register(models.ContactInfo)
+admin.site.register(models.Dis_Transaction)
+admin.site.register(models.Distributor)
+admin.site.register(models.Favorite)
+admin.site.register(models.Review)
+admin.site.register(models.Sale)
+admin.site.register(models.Worker)
