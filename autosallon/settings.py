1f9171b789067d0a7b83c366ecfba2bba6bcb0da
gemata <bp55994@ubt-uni.net>
2023-06-07 12:37:11 +0200

    Added Pagination to Car Page

--- autosallon/settings.py
+++ autosallon/settings.py
@@ -347,6 +347,9 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
     ),
+    
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "PAGE_SIZE":12
 }
 
 # JWT Settings
--- reactapp/src/pages/Cars.js
+++ reactapp/src/pages/Cars.js
@@ -7,22 +7,35 @@ import ExtendedFilter from "../components/carComponents/extendedFilter";
 
 export default function Cars() {
 
-  const [data, getData] = useState([])
+  const [data, getData] = useState([]);
+  const [page, setPage] = useState(0); // Current page number
+  const [perPage, setPerPage] = useState(12); // Number of cars per page
+  
   let API = 'http://127.0.0.1:8000/prova/car/?search=';
   const fetchData = () => {
+    
+    if(page < 0){
+      setPage(0);
+    }
+    // const url = `${API}?page=${page}&limit=${perPage}`;
+    API = "http://127.0.0.1:8000/prova/car/?limit=12&offset=" + (12*page);
+    console.log(API)
     fetch(API)
       .then((res) => res.json())
       .then((res) => {
-        getData(res)
-      })
-  }
+        getData(res.results);
+        console.log(res.results);
+      });
+  };
 
   useEffect(() => {
     fetchData()
-  }, [])
+  }, [page, perPage])
 
   function search(e) {
     e.preventDefault();
+
+    setPage(0);
 
     let search;
 
@@ -36,12 +49,14 @@ export default function Cars() {
 
     let data = make + " " + model + " " + category + " " + year + color + mileage + price;
 
-    let API = 'http://127.0.0.1:8000/prova/car/?search=' + data;
+    API = "http://127.0.0.1:8000/prova/car/?limit=12&offset=" + (12*page) + "&search=" + data;
+
+    console.log(API)
     const fetchSearchData = () => {
       fetch(API)
         .then((res) => res.json())
         .then((res) => {
-          getData(res)
+          getData(res.results)
         })
     }
     fetchSearchData();
@@ -67,6 +82,13 @@ export default function Cars() {
         {data.map((item, i) => {
           return <CarItem key={item.id} id={item.id} image={item.imageName} logo={item.make} name={item.make + " " + item.model} year={item.year} mileage={item.mileage} />
         })}
+
+      <div className="pagination">
+        <button onClick={() => setPage(page - 1)}>
+          Previous
+        </button>
+        <button onClick={() => setPage(page + 1)}>Next</button>
+      </div>
       </section>
 
       <Categories />
