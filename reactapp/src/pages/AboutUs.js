5dc14eae1895203d91a459f500ad6d7d08faa691
gemata <bp55994@ubt-uni.net>
2023-06-07 12:39:27 +0200

    about contact

--- reactapp/src/pages/AboutUs.js
+++ reactapp/src/pages/AboutUs.js
@@ -2,39 +2,84 @@ import { React } from "react";
 import Footer from '../components/footer'
 import "../index.css";
 
-export default function AboutUs() {
-    return (
-      <>
-        <div className="about-us-container">
-          <h1 className="about-us-title">About Us</h1>
-          <p className="about-us-description">
-            Welcome to our car showroom! We are passionate about cars and strive
-            to provide the best selection and service to our customers. With a
-            wide range of vehicles, including sedans, SUVs, trucks, and luxury
-            cars, we have something to suit every preference and budget. Whether
-            you're a first-time buyer or an avid car enthusiast, our showroom is
-            the perfect place to explore and find your dream car.
-          </p>
-          <p className="about-us-mission">
-            Our mission is to deliver exceptional customer satisfaction by
-            offering top-quality vehicles, transparent pricing, and a seamless car
-            buying experience. We carefully handpick each vehicle in our
-            inventory, ensuring that they meet our high standards of quality and
-            performance. Our knowledgeable and friendly staff are here to assist
-            you every step of the way, from providing detailed vehicle
-            information to arranging test drives and helping with financing
-            options.
-          </p>
-          <p className="about-us-visit">
-            Visit our showroom today and experience the thrill of owning a
-            high-quality car. We are dedicated to making your car buying journey
-            enjoyable and memorable. Feel free to browse our inventory online or
-            contact our team for personalized assistance. We look forward to
-            serving you and helping you find the perfect car that matches your
-            style and needs.
-          </p>
+import Aboutimg from '../images/heroImage.webp'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import { BiBook } from 'react-icons/bi';
+import  { useState, useEffect } from 'react';
+
+function AboutUS() {
+  const [isHovered, setIsHovered] = useState(false);
+  const [text, setText] = useState('');
+
+  const fullText = "Car Show Room";
+
+  useEffect(() => {
+    let currentText = '';
+    let index = 0;
+    let typingInterval;
+
+    if (isHovered) {
+      typingInterval = setInterval(() => {
+        currentText += fullText[index];
+        setText(currentText);
+        index++;
+
+        if (index >= fullText.length) {
+          clearInterval(typingInterval);
+        }
+      }, 150);
+    } else {
+      clearInterval(typingInterval);
+      setText('');
+    }
+
+    return () => clearInterval(typingInterval);
+  }, [isHovered]);
+
+  return (
+    <section id='about'>
+      <h5>get to</h5>
+      <h2>Know me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+        <div className="about__me-image"onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
+<img src={Aboutimg} alt='hello'></img>
+<div className="about__me-text">{text}</div>
         </div>
-        <Footer />
-      </>
-    );
-  }
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <FaAward className="about__icon"/>
+              <h5>experience</h5>
+              <small>5+ Years working </small>
+
+            </article>
+            <article className='about__card'>
+              <FiUsers className="about__icon"/>
+              <h5>clients</h5>
+              <small>10,000 + lcients worldwide</small>
+
+            </article>
+            <article className='about__card'>
+              <BiBook className="about__icon"/>
+              <h5>Projects</h5>
+              <small>Over 18,000 done projects</small>
+
+            </article>
+
+          </div>
+          <p>Car Show Room</p>
+        <a href='http://127.0.0.1:8000/chatGPT/' className='btn btn-primary'> let's talk</a>
+        </div>
+      </div>
+      <Footer />
+    </section>
+    
+    
+  )
+}
+
+export default AboutUS
--- reactapp/src/pages/Contact.js
+++ reactapp/src/pages/Contact.js
@@ -1,14 +1,56 @@
 import { React } from "react";
 import Footer from '../components/footer'
-import Header from "../components/header";
+import "../index.css";
 
 
+import { BsWhatsapp } from 'react-icons/bs';
+import {MdOutlineMailOutline} from 'react-icons/md';
+import { useRef } from 'react';
+import emailjs from 'emailjs-com';
 
-export default function Contact() {
+function Contact() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_logddqq', 'template_cgcoypa', form.current, 'EzLyRehMqd1CzThPB')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
   return (
-    <>
-      <Header />
-      <Footer />
-    </>
+    <section id='contact'>
+     
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+      <div className='container contact__container'>
+        <div className='contact__options'>
+          <article className="contact__option">
+            <MdOutlineMailOutline className='contact__option-icon'/>
+            <h4>Email</h4>
+            <h5>autoparadise@gmail.com</h5>
+            <a href='mailto:brunomike058@gmail.com'>Send a message </a>
+          </article>
+          <article className="contact__option">
+            <BsWhatsapp className='contact__option-icon'/>
+            <h4>whatsapp</h4>
+            <h5>+38345880254</h5>
+            <a href='https://wa.me/+237682370254'>Send a message </a>
+          </article>
+        </div>
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name='name' placeholder="Name"required></input>
+          <input type="email" name='email' placeholder="email"required></input>
+          <textarea name='message' placeholder="Message" required></textarea>
+          <button type="submit"className='btn btn primary'>send Message</button>
+        </form>
+        </div>
+        <Footer/>
+    </section>
   )
 }
+
+export default Contact
